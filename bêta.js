3ff119253fd8020e023374fa76cf259197278b52
Alohn2267 <76687427+Alohn2267@users.noreply.github.com>
2021-11-12 22:18:52 +0100

    Commentaires + problèmes esthétiques

--- bêta.js
+++ bêta.js
@@ -6,10 +6,9 @@ function load() {
         $(".circle_animation_h").css("transition", "all 3600s linear");
         $(".circle_animation_min").css("transition", "all 60s linear");
     }, 500);
-    print("hElllllll")
 }
 load()
-// PRINT
+// PRINT (pcq c'est + rapide)
 function print(args) {
     console.log(args)
 }
@@ -26,6 +25,7 @@ function move(end, beginning, now) {
     if (i == 0) {
         i = 1;
         var elem = document.getElementById("myBar");
+        var txt = document.getElementById("txt_bar")
         frame()
         var id = setInterval(frame, 10000);
         function frame() {
@@ -38,108 +38,72 @@ function move(end, beginning, now) {
             clearInterval(id);
             i = 0;
             } else {
-            width = calcul1;
+            var width = calcul1;
             elem.style.width = width + "%";
-            elem.innerHTML = width  + "% de déjà fait !";
+            txt.innerHTML = width  + "% de déjà fait !";
             }
         }
     }
 }
-move(1634917500, 1630569600, (Date.now()/1000))
-
+move(1639751100000, 1636354800000, (Date.now()))
+// move(1636751100000, 1636354800000, (Date.now()))
 // Compte à rebours
 function compte_a_rebours()
 {
     var compte_a_rebours = document.getElementById("compte_a_rebours");
 
     var date_actuelle = new Date();
-    var date_evenement = new Date("Oct 22 15:45:00 2021");
+    var date_evenement = new Date("Dec 17 15:25:00 2021");
     var total_secondes = (date_evenement - date_actuelle) / 1000;
-    if(date_actuelle >= date_evenement) {
-        aligner.innerHTML = "En vacances !";
-        return;
-    }
+    // if(date_actuelle >= date_evenement) {
+    //     aligner.innerHTML = "En vacances !";
+    //     return;
+    // }
 
     if (total_secondes > 0)
     {
+        //Init tous les nbs
         var jours = Math.floor(total_secondes / (60 * 60 * 24));
         var heures = Math.floor((total_secondes - (jours * 60 * 60 * 24)) / (60 * 60));
-        minutes = Math.floor((total_secondes - ((jours * 60 * 60 * 24 + heures * 60 * 60))) / 60);
-        secondes = Math.floor(total_secondes - ((jours * 60 * 60 * 24 + heures * 60 * 60 + minutes * 60)));
-
-        var et = "et";
+        var minutes = Math.floor((total_secondes - ((jours * 60 * 60 * 24 + heures * 60 * 60))) / 60);
+        var secondes = Math.floor(total_secondes - ((jours * 60 * 60 * 24 + heures * 60 * 60 + minutes * 60)));
+        // Init tous les mots (avec des "s" dès le début)
         var mot_jour = " jours";
         var mot_heure = " heures";
         var mot_minute = " minutes";
         var mot_seconde = " secondes";
 
-        if (jours == 0)
-        {
-            jours = '';
-            mot_jour = '';
-        }
-        else if (jours == 1)
-        {
-            mot_jour = " jour";
-        }
+        // Enlève le "s" si il n'y en a qu'un
+        if (jours <= 1){mot_jour = " jour";}
+        
+        // Enlève le "s" si il n'y en a qu'un ou zero
+        if (heures <= 1){mot_heure = " heure";}
 
-        if (heures == 0)
-        {
-            heures = '';
-            mot_heure = '';
-        }
-        else if (heures == 1)
-        {
-            mot_heure = " heure";
-        }
+        // Enlève le "s" si il n'y en a qu'un
+        if (minutes <= 1){mot_minute = " minute";}
 
-        if (minutes == 0)
-        {
-            minutes = '';
-            mot_minute = '';
-        }
-        else if (minutes == 1)
-        {
-            mot_minute = " minute";
-        }
-
-        if (secondes == 0)
-        {
-            secondes = '';
-            mot_seconde = '';
-            et = '';
-        }
-        else if (secondes == 1)
-        {
-            mot_seconde = " seconde";
-        }
-
-        if (minutes == 0 && heures == 0 && jours == 0)
-        {
-            et = "";
-        }
-
-        // jour.innerHTML = jours + mot_jour + "&nbsp;"
-        // heure.innerHTML = heures + mot_heure + "&nbsp;"
-        // minute.innerHTML = minutes + mot_minute + "&nbsp;"
-        // seconde.innerHTML = secondes + mot_seconde + "&nbsp;"
+        // Enlève le "s" si il n'y en a qu'un
+        if (secondes <= 1){mot_seconde = " seconde";}
     }
-    /* Need initial run as interval hasn't yet occured... */
+
     // Cercles secondes
     $('.circle_animation_sec').css('stroke-dashoffset', initialOffset-(1*(initialOffset/time)));
             $('.seconde_h3').text(secondes);
             $('.mot_seconde_h3').text(mot_seconde);
         $('.circle_animation_sec').css('stroke-dashoffset', initialOffset-((secondes)*(initialOffset/time)));
-    // Cercles minutes
+    
+        // Cercles minutes
     $('.circle_animation_min').css('stroke-dashoffset', initialOffset-(1*(initialOffset/time)));
             $('.min_h3').text(minutes);
             $('.mot_min_h3').text(mot_minute);
         $('.circle_animation_min').css('stroke-dashoffset', initialOffset-((minutes)*(initialOffset/time)));
+    
     // Cercles heures
     $('.circle_animation_h').css('stroke-dashoffset', initialOffset-(1*(initialOffset/time)));
             $('.heure_h3').text(heures);
             $('.mot_heure_h3').text(mot_heure);
         $('.circle_animation_h').css('stroke-dashoffset', initialOffset-((heures)*(initialOffset/time)));
+    
     // Cercles jours
     $('.circle_animation_j').css('stroke-dashoffset', initialOffset-(1*(initialOffset/time)));
             $('.jour_h3').text(jours);
@@ -179,10 +143,9 @@ function compte_a_rebours()
 }
 
 function append_txt(arg){
-    print(arg)
     if(arg==0){
         $("body").append(clone_suppr);
     }
 }
-console.log("Hello")
+console.log("Js lié")
 compte_a_rebours();
